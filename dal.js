8b6814ca47c6b718827c410e53d0054aa5ab86cd
digby-j <developer@jakedigby.com>
2022-06-10 12:33:37 -0400

    Connected ObjectRoecket DB to frontend

--- dal.js
+++ dal.js
@@ -1,17 +1,43 @@
 const { rejects } = require('assert');
 
 const MongoClient   = require('mongodb').MongoClient;
-const url           = 'mongodb://localhost:27017';
-let db              = null;
+// const url           = 'mongodb://localhost:27017';
+// let db              = null;
 
-// connect mongodb
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client){
-    console.log("Connected successfully to db server");
-    console.log(err);
+// // connect mongodb
+// MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client){
+//     console.log("Connected successfully to db server");
+//     console.log(err);
 
-    // connect to myproject database
-    db = client.db('myproject');
+//     // connect to myproject database
+//     db = client.db('myproject');
+// });
+
+
+
+// Ensure you have run 'npm install mongodb'
+var MongoClient = require('mongodb').MongoClient;
+
+var username = `${process.env.DB_NAME}`;
+var password = `${process.env.DB_PASS}`;
+var hosts = `${process.env.DB_HOST}`;
+var database = `${process.env.DB_DATABASE}`;
+var options = `${process.env.DB_OPTIONS}`;
+var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
+
+MongoClient.connect(connectionString, function(err, db) {
+    if (db) {
+        db.close();
+    }
+    if (err) {
+        console.log('Error: ', err);
+    } else {
+        console.log('Connected!');
+        process.exit();
+    }
 });
+
+
 
 // create user account
 function create(name, email, password){
