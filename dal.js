0f2b99573b35ff826c177325b597f4019af42c4a
digby-j <developer@jakedigby.com>
2022-06-11 16:40:52 -0400

    Fixed Heroku db connect

--- dal.js
+++ dal.js
@@ -1,12 +1,158 @@
+// const { rejects } = require('assert');
+// require('dotenv').config();
+
+// // const MongoClient   = require('mongodb').MongoClient;
+// // const url           = 'mongodb://localhost:27017';
+// // let db              = null;
+
+// // Ensure you have run 'npm install mongodb'
+// var MongoClient = require('mongodb').MongoClient;
+
+// // var username = `${process.env.DB_NAME}`;
+// // var password = `${process.env.DB_PASS}`;
+// // var hosts = `${process.env.DB_HOST}`;
+// // var database = `${process.env.DB_DATABASE}`;
+// // var options = `${process.env.DB_OPTIONS}`;
+// // var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
+
+
+// var username = process.env.DB_USER;
+// var password = process.env.DB_PASS;
+// var hosts = process.env.DB_HOST;
+// var database = process.env.DB_DATABASE;
+// var options = process.env.DB_OPTIONS;
+// var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
+
+// var db;
+// var error;
+// var waiting = [];
+
+// MongoClient.connect(connectionString, function(err, db) {
+//     error = err;
+//     db = database;
+  
+//     waiting.forEach(function(callback) {
+//       callback(err, database);
+//     });
+//   });
+  
+//   module.exports = function(callback) {
+//     if (db || error) {
+//       callback(error, db);
+//     } else {
+//       waiting.push(callback);
+//     }
+//   }
+//     // if (db) {
+//     //     db.close();
+//     // }
+//     // if (err) {
+//     //     console.log('Error: ', err);
+//     // } else {
+//     //     console.log('Connected!');
+//     //     process.exit();
+//     // }
+// // });
+
+
+
+// // create user account
+// function create(name, email, password){
+//     return new Promise((resolve, reject) => {
+//         // const collection = db.collection('users');
+//         const collection = db.collection('usersDetails');
+//         const doc = {name, email, password, balance: 0};
+//         collection.insertOne(doc, {w:1}, function(err, result) {
+//             err ? reject(err) : resolve(doc);
+//         });
+//     })
+// }
+
+// function find(email) {
+//     return new Promise((resolve, reject) => {
+//         const customers = db
+//             // .collection('users')
+//             .collection('usersDetails')
+//             .find({email: email})
+//             .toArray(function (err, docs) {
+//                 err ? reject(err) : resolve(docs);
+//             });
+//     })
+
+// }
+
+// // update - deposit/withdraw amount
+// function update(email, amount) {
+//     return new Promise((resolve, reject) => {
+//         const customers = db
+//             // .collection('users')
+//             .collection('usersDetails')
+//             .findOneAndUpdate(
+//                 { email: email },
+//                 { $inc: { balance: amount } },
+//                 { returnOriginal: false },
+//                 function (err, documents) {
+//                     err ? reject(err) : resolve(documents);
+//                 }
+//             );
+
+
+//     });
+// }
+
+// function findOne(email) {
+//     return new Promise((resolve, reject) => {
+//         const customers = db
+//             // .collection('users')
+//             .collection('usersDetails')
+//             .findOne({ email: email })
+//             .then((doc) => resolve(doc))
+//             .catch((err) => reject(err));
+//     })
+// }
+
+
+//  // all users
+//  function all(){
+//      return new Promise((resolve, reject) => {
+//          const customers = db
+//         //  .collection('users')
+//          .collection('usersDetails')
+//          .find({})
+//          .toArray(function(err, docs) {
+//              err ? reject(err) : resolve(docs);
+//          });
+         
+//      })
+//  }
+
+//  module.exports = {create, findOne, find, update, all};
+
 const { rejects } = require('assert');
-require('dotenv').config();
 
 // const MongoClient   = require('mongodb').MongoClient;
 // const url           = 'mongodb://localhost:27017';
 // let db              = null;
 
+// // connect mongodb
+// MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client){
+//     console.log("Connected successfully to db server");
+//     console.log(err);
+
+//     // connect to myproject database
+//     db = client.db('myproject');
+// });
+
+
+
 // Ensure you have run 'npm install mongodb'
 var MongoClient = require('mongodb').MongoClient;
+
+var username = process.env.DB_USER;
+var password = process.env.DB_PASS;
+var hosts = process.env.DB_HOST;
+var database = process.env.DB_DATABASE;
+var options = process.env.DB_OPTIONS;
 
 // var username = `${process.env.DB_NAME}`;
 // var password = `${process.env.DB_PASS}`;
@@ -14,13 +160,6 @@ var MongoClient = require('mongodb').MongoClient;
 // var database = `${process.env.DB_DATABASE}`;
 // var options = `${process.env.DB_OPTIONS}`;
 // var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
-
-var username = process.env.DB_USER;
-var password = process.env.DB_PASS;
-var hosts = process.env.DB_HOST;
-var database = process.env.DB_DATABASE;
-var options = process.env.DB_OPTIONS;
-var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
 
 MongoClient.connect(connectionString, function(err, db) {
     if (db) {
@@ -39,8 +178,7 @@ MongoClient.connect(connectionString, function(err, db) {
 // create user account
 function create(name, email, password){
     return new Promise((resolve, reject) => {
-        // const collection = db.collection('users');
-        const collection = db.collection('usersDetails');
+        const collection = db.collection('users');
         const doc = {name, email, password, balance: 0};
         collection.insertOne(doc, {w:1}, function(err, result) {
             err ? reject(err) : resolve(doc);
@@ -51,8 +189,7 @@ function create(name, email, password){
 function find(email) {
     return new Promise((resolve, reject) => {
         const customers = db
-            // .collection('users')
-            .collection('usersDetails')
+            .collection('users')
             .find({email: email})
             .toArray(function (err, docs) {
                 err ? reject(err) : resolve(docs);
@@ -65,8 +202,7 @@ function find(email) {
 function update(email, amount) {
     return new Promise((resolve, reject) => {
         const customers = db
-            // .collection('users')
-            .collection('usersDetails')
+            .collection('users')
             .findOneAndUpdate(
                 { email: email },
                 { $inc: { balance: amount } },
@@ -83,8 +219,7 @@ function update(email, amount) {
 function findOne(email) {
     return new Promise((resolve, reject) => {
         const customers = db
-            // .collection('users')
-            .collection('usersDetails')
+            .collection('users')
             .findOne({ email: email })
             .then((doc) => resolve(doc))
             .catch((err) => reject(err));
@@ -96,8 +231,7 @@ function findOne(email) {
  function all(){
      return new Promise((resolve, reject) => {
          const customers = db
-        //  .collection('users')
-         .collection('usersDetails')
+         .collection('users')
          .find({})
          .toArray(function(err, docs) {
              err ? reject(err) : resolve(docs);
--- mongo_test.js
+++ mongo_test.js
@@ -1,128 +1,203 @@
-// const MongoClient = require('mongodb').MongoClient;
-// const url = 'mongodb://localhost:27017';
+// // const MongoClient = require('mongodb').MongoClient;
+// // const url = 'mongodb://localhost:27017';
 
-// MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-//     console.log('Connected!');
+// // MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+// //     console.log('Connected!');
 
-//     // database name
-//     const dbName = 'myproject';
-//     const db = client.db(dbName);
+// //     // database name
+// //     const dbName = 'myproject';
+// //     const db = client.db(dbName);
 
-//     // new user
-//     var name = 'user' + Math.floor(Math.random()*10000);
-//     var email = name + '@jakedigby.com';
+// //     // new user
+// //     var name = 'user' + Math.floor(Math.random()*10000);
+// //     var email = name + '@jakedigby.com';
 
-//     // Insert into customer table
-//     var collection = db.collection('customers');
-//     var doc = {name, email};
-//     collection.insertOne(doc, {w:1}, function(err, result) {
-//         console.log('Document Inserted');
-//     });
-// });
+// //     // Insert into customer table
+// //     var collection = db.collection('customers');
+// //     var doc = {name, email};
+// //     collection.insertOne(doc, {w:1}, function(err, result) {
+// //         console.log('Document Inserted');
+// //     });
+// // });
 
 
-//Ensure you have run 'npm install mongodb'
-var MongoClient = require('mongodb').MongoClient;
-
-var username = 'jdigby';
-var password = 'aGYxoWdKNi4zrq8sWaAwwKbM';
-var hosts = 'iad2-c6-2.mongo.objectrocket.com:53121,iad2-c6-0.mongo.objectrocket.com:53121,iad2-c6-1.mongo.objectrocket.com:53121';
-var database = 'users';
-var options = '?replicaSet=1ec981ff3bbe4194ac37f886824c7e20';
-var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
-
+// //Ensure you have run 'npm install mongodb'
 // var MongoClient = require('mongodb').MongoClient;
 
-// var username = `${process.env.DB_NAME}`;
-// var password = `${process.env.DB_PASS}`;
+// var username = 'jdigby';
+// var password = 'aGYxoWdKNi4zrq8sWaAwwKbM';
 // var hosts = 'iad2-c6-2.mongo.objectrocket.com:53121,iad2-c6-0.mongo.objectrocket.com:53121,iad2-c6-1.mongo.objectrocket.com:53121';
-// var database = `${process.env.DB_DATABASE}`;
+// var database = 'users';
 // var options = '?replicaSet=1ec981ff3bbe4194ac37f886824c7e20';
 // var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
 
+// // var MongoClient = require('mongodb').MongoClient;
+
+// // var username = `${process.env.DB_NAME}`;
+// // var password = `${process.env.DB_PASS}`;
+// // var hosts = 'iad2-c6-2.mongo.objectrocket.com:53121,iad2-c6-0.mongo.objectrocket.com:53121,iad2-c6-1.mongo.objectrocket.com:53121';
+// // var database = `${process.env.DB_DATABASE}`;
+// // var options = '?replicaSet=1ec981ff3bbe4194ac37f886824c7e20';
+// // var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
+
+// // MongoClient.connect(connectionString, function(err, db) {
+// //     if (db) {
+// //         db.close();
+// //     }
+// //     if (err) {
+// //         console.log('Error: ', err);
+// //     } else {
+// //         console.log('Connected!');
+// //         process.exit();
+// //     }
+// //          //database name
+// //     // const dbName = 'users';
+// //     // const db = client.db(dbName);
+
+// //     // new user
+// //     var name = 'user' + Math.floor(Math.random()*10000);
+// //     var email = name + '@jakedigby.com';
+
+// //     // Insert into customer table
+// //     var collection = db.collection('userDetails');
+// //     var doc = {name, email};
+// //     collection.insertOne(doc, {w:1}, function(err, result) {
+// //         console.log('Document Inserted');
+// //     });
+// // });
+
+// // Connect
 // MongoClient.connect(connectionString, function(err, db) {
-//     if (db) {
-//         db.close();
-//     }
 //     if (err) {
-//         console.log('Error: ', err);
-//     } else {
-//         console.log('Connected!');
-//         process.exit();
+//       console.log('Error: ', err);
 //     }
-//          //database name
-//     // const dbName = 'users';
-//     // const db = client.db(dbName);
-
-//     // new user
-//     var name = 'user' + Math.floor(Math.random()*10000);
-//     var email = name + '@jakedigby.com';
-
-//     // Insert into customer table
-//     var collection = db.collection('userDetails');
-//     var doc = {name, email};
-//     collection.insertOne(doc, {w:1}, function(err, result) {
-//         console.log('Document Inserted');
+//     var example_doc = {
+//           "start" : new Date(),
+//           "end" : new Date(2015, 9, 28, 14, 17, 23, 0),
+//           "location" : "Texas",
+//           "official_game" : false,
+//           "winner" : "Javi",
+//           "players" : [
+//           {
+//               "name": "Javi",
+//               "decks": [
+//                   "Dinosaurs",
+//                   "Plants"
+//               ],
+//               "points": 24,
+//               "place": 1
+//           },
+//           {
+//               "name": "Seth",
+//               "decks": [
+//                   "Spies",
+//                   "Zombies"
+//               ],
+//               "points": 20,
+//               "place": 2
+//           },
+//           {
+//               "name": "Dave",
+//               "decks": [
+//                   "Steampunk",
+//                   "Wizard"
+//               ],
+//               "points": 20,
+//               "place": 2
+//           },
+//           {
+//               "name": "Castro",
+//               "decks": [
+//                   "Shapeshifters",
+//                   "Ninjas"
+//               ],
+//               "points": 18,
+//               "place": 4
+//           }
+//       ]
+//     };
+//     var collection = db.collection('objectrocket.init');
+//     collection.insert(example_doc, {w:1}, function(err, result) {
+//       if (err) {
+//         console.log('Error: ', err);
+//       } else {
+//         console.log('Inserted a doc!');
+//         process.exit();
+//       }
 //     });
-// });
+//   });
+
+const mongoose = require('mongoose');
+// Require mongodb
+var MongoClient = require('mongodb').MongoClient;
 
 // Connect
-MongoClient.connect(connectionString, function(err, db) {
+MongoClient.connect("mongodb://jdigby:aGYxoWdKNi4zrq8sWaAwwKbM@iad2-c6-2.mongo.objectrocket.com:53121,iad2-c6-0.mongo.objectrocket.com:53121,iad2-c6-1.mongo.objectrocket.com:53121/users?ssl=true", function(err, db) {
+  if (err) {
+    return console.dir(err);
+  }
+  var example_doc = {
+        "start" : new Date(),
+        "end" : new Date(2015, 9, 28, 14, 17, 23, 0),
+        "location" : "Texas",
+        "official_game" : false,
+        "winner" : "Javi",
+        "players" : [
+        {
+    "name": "Javi",
+            "decks": [
+                "Dinosaurs",
+                "Plants"
+            ],
+            "points": 24,
+            "place": 1
+        },
+        {
+            "name": "Seth",
+            "decks": [
+                "Spies",
+                "Zombies"
+            ],
+            "points": 20,
+            "place": 2
+        },
+        {
+            "name": "Dave",
+            "decks": [
+                "Steampunk",
+                "Wizard"
+            ],
+            "points": 20,
+            "place": 2
+        },
+        {
+            "name": "Castro",
+            "decks": [
+                "Shapeshifters",
+                "Ninjas"
+            ],
+            "points": 18,
+            "place": 4
+        }
+    ]
+  };
+  
+  var collection = db.collection('example_collection');
+  collection.insert(example_doc, {w:1}, function(err, result) {
     if (err) {
-      console.log('Error: ', err);
+      return console.dir(err);
+    } else {
+      console.log("Inserted a doc!");
+      process.exit();
     }
-    var example_doc = {
-          "start" : new Date(),
-          "end" : new Date(2015, 9, 28, 14, 17, 23, 0),
-          "location" : "Texas",
-          "official_game" : false,
-          "winner" : "Javi",
-          "players" : [
-          {
-              "name": "Javi",
-              "decks": [
-                  "Dinosaurs",
-                  "Plants"
-              ],
-              "points": 24,
-              "place": 1
-          },
-          {
-              "name": "Seth",
-              "decks": [
-                  "Spies",
-                  "Zombies"
-              ],
-              "points": 20,
-              "place": 2
-          },
-          {
-              "name": "Dave",
-              "decks": [
-                  "Steampunk",
-                  "Wizard"
-              ],
-              "points": 20,
-              "place": 2
-          },
-          {
-              "name": "Castro",
-              "decks": [
-                  "Shapeshifters",
-                  "Ninjas"
-              ],
-              "points": 18,
-              "place": 4
-          }
-      ]
-    };
-    var collection = db.collection('objectrocket.init');
-    collection.insert(example_doc, {w:1}, function(err, result) {
-      if (err) {
-        console.log('Error: ', err);
-      } else {
-        console.log('Inserted a doc!');
-        process.exit();
-      }
-    });
   });
+
+  collection.findOne({"winner":"Javi"},function(err, doc) {
+    if (err) {
+      return console.dir(err);
+    } else {
+      console.log(prettyjson.render(doc));
+      process.exit();
+    }
+  });
+});
